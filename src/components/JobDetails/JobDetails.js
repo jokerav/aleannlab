8356b67565afb4d5baae522271e2e368cdcbe8ab
jokerav <jokeravdeveloper@gmail.com>
2022-12-10 18:24:18 +0200

    123

--- src/components/JobDetails/JobDetails.js
+++ src/components/JobDetails/JobDetails.js
@@ -1,5 +1,6 @@
 import {useParams, Link} from 'react-router-dom';
 import {useGetDataQuery} from '../../store/dataApi';
+import {differenceInMonths} from 'date-fns'
 import {ReactComponent as BookMark} from '../../img/Rectangle 31.svg';
 import {ReactComponent as Star} from '../../img/star.svg';
 import {ReactComponent as Share} from '../../img/Share.svg';
@@ -13,21 +14,41 @@ const JobDetails = () => {
     const {jobId} = useParams();
     const [currentJob] = data.filter(job => job.id === jobId);
     console.log(currentJob);
-    const {description} = currentJob;
+    const {title, createdAt, salary, description} = currentJob;
+    const difference = differenceInMonths(new Date(), new Date(createdAt));
     return (
         <div className='containerDetails'>
             <div className='headerContainer'>
-            <p className='jobDetails'>Job Details</p>
-            <hr className='lineMobile'/>
-            <div className='shareContainer'>
-                <BookMark className='bookMarkDetail'/>
-                <Star className='star'/>
-                <p className='saveToMyList'>Save to my list</p>
-                <Share className='share'/>
-                <p className='share'>Share</p>
-            </div>
+                <p className='jobDetails'>Job Details</p>
+                <hr className='lineMobile'/>
+                <div className='shareContainer'>
+                    <BookMark className='bookMarkDetail'/>
+                    <Star className='star'/>
+                    <p className='saveToMyList'>Save to my list</p>
+                    <Share className='share'/>
+                    <p className='share'>Share</p>
+                </div>
             </div>
             <hr className='lineDesctop'/>
+            <button type='button' className='btnApplyDesctop'>Apply now</button>
+            <div className='titleDivMobile'>
+                <p className='title'>{title}</p>
+                <div className="postedSalaryDivMobile">
+                    <p className='posted'>Posted {difference} months ago</p>
+                    <div className='salaryDiv'>
+                        <p className='brutto'>Brutto, per year</p>
+                        <p className='salary'>{salary}</p>
+                    </div>
+                </div>
+            </div>
+            <div className='titleDivDesctop'>
+                <p className='title'>{title}</p>
+                <div className='salaryDiv'>
+                    <p className='salary'>{salary}</p>
+                    <p className='brutto'>Brutto, per year</p>
+                </div>
+            </div>
+            <p className='postedDesctop'>Posted {difference} months ago</p>
 
 
             <p>{description}</p>
